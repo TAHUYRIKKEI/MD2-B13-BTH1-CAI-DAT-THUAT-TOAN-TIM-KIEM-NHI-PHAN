60811d493e580e9fde2ffa73b80a644266030448
unknown <tavanhuyvp8590@gmail.com>
2022-08-09 21:41:12 +0700

    BTH2

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import static java.util.Arrays.binarySearch;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(binarySearch(BinarySearch.list,2));
+        System.out.println(binarySearch(BinarySearch.list,11));
+        System.out.println(binarySearch(BinarySearch.list,79));
+        System.out.println(binarySearch(BinarySearch.list,1));
+        System.out.println(binarySearch(BinarySearch.list,5));
+        System.out.println(binarySearch(BinarySearch.list,80));
+
+    }
+}
